81042f9ac46c0da916c3c6db3896fdd8034ecff2
Abanoub Halim <Abanoub.Halim@linkdev.com>
2024-12-14 19:29:11 +0200

    commit

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -12,16 +12,10 @@ import { CommonModule } from '@angular/common';
 export class ModalComponent {
   @Output() close = new EventEmitter<void>();
   images: string[] = [
-    '/assets/images/1.jpg',
-    '/assets/images/2.jpg',
-    '/assets/images/3.jpg',
-    '/assets/images/4.jpg',
-    '/assets/images/5.jpg',
-    '/assets/images/6.jpg',
-    '/assets/images/7.jpg',
-    '/assets/images/8.jpg',
-    '/assets/images/9.jpg',
-    '/assets/images/10.jpg'
+    '/assets/images/floor1.jpg',
+    '/assets/images/Church.jpg',
+    '/assets/images/Garage.jpg',
+    '/assets/images/Room.jpg',
   ];
   activeSlideIndex = 0;
   
